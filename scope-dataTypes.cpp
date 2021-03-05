5f32e930d27b2f1132c128ed5410c0d826115022
rahul1237 <rahulmahajan98645@gmail.com>
2021-03-06 03:34:50 +0530

    added

---
+++ scope-dataTypes.cpp
@@ -0,0 +1,25 @@
+ #include<iostream>
+ using namespace std;
+
+ int main(){
+    int a=5 , b=10;
+    float c=1.5;
+    char d='r';
+    bool istrue=true;
+    bool isfalse=false;
+
+// int values
+    cout<<"value of a is: "<<a<<endl;
+    cout<<"value of b is: "<<b<<endl;
+
+// float values
+    cout<<"value of c is: "<<c<<endl;
+    cout<<"value of d is: "<<d<<endl;
+
+// bool values
+    cout<<"value of istrue is: "<<istrue<<endl;
+    cout<<"value of isflase is: "<<isfalse<<endl;
+    // true returns 1 while false return 0
+
+    return 0;
+ }
---
+++ var-com.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num = 10;
+    cout<<num<<endl;
+    cout<<"----"<<endl;
+    cout<<num*10<<endl;
+    return 0;
+}
